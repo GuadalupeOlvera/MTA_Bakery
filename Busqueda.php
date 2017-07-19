1d9af5efa4f4a0d96ef1bee72db8c674f7faefc4
GuadalupeOlvera <zolgpe@gmail.com>
2017-07-19 16:31:25 -0500

    Add files via upload

---
+++ Busqueda.php
@@ -0,0 +1,22 @@
+<?php
+	$busca="";
+	$busca=$_POST('busca');
+	mysql_connect("localhost", "root");
+	mysql_select_db(/*database_name*/);
+	if ($busca!="") {
+		$busqueda=mysql_query("select * from Productos where Nombre_Producto like '%".$busca."%'");
+	}
+?>
+
+<table width="805" border="1">
+	<tr>
+		<td width="225"></td>
+	</tr>
+</table>
+
+<?php
+	while ($muestra = mysql_fetch_array($busqueda)) {
+		echo "<tr>";
+		echo "<td>".$muestra['/*elemento_buscado*/'].'</td>';
+	}
+?>
